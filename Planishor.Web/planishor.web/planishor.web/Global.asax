fec124c16d5512bf593129cf82769c81ef9c20eb
visrosoftware <visrosoftware@gmail.com>
2018-02-18 00:32:35 +0530

    planshor web and front end

---
+++ Planishor.Web/planishor.web/planishor.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="planishor.web.MvcApplication" Language="C#" %>
---
+++ Planishor.Web/planishor.web/planishor.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(planishor.web.Startup))]
+namespace planishor.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
